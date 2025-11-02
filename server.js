a41c86ea91b81cc877f38af710d01fc63059ec7a
mirianukeje <ukejemirian@gmail.com>
2025-11-02 20:35:15 +0100

    Week 1

--- server.js
+++ server.js
@@ -6,14 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function (_req, res){
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
@@ -29,7 +43,3 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-// Index route
-app.get("/", function (_req, res){
-  res.render("index", { title: "Home" })
-})
